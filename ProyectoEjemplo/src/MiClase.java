71dcd01df471226e64ba36719edc6b9ccda093eb
Joseba <joseba.rodriguez01@gmail.com>
2021-01-28 17:29:17 +0100

    Un pequeño cambio

--- ProyectoEjemplo/src/MiClase.java
+++ ProyectoEjemplo/src/MiClase.java
@@ -3,7 +3,7 @@ public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola, esto es un mensaje");
+		System.out.println("Hola, esto es un nuevo mensaje");
 	}
 
 }
