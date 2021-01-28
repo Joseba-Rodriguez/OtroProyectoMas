8d7a8aa859cca4ee7ae88dc6b4cb52331e71cafd
Joseba <joseba.rodriguez01@gmail.com>
2021-01-28 17:33:28 +0100

    otro cambio de rama

--- ProyectoEjemplo/src/MiClase.java
+++ ProyectoEjemplo/src/MiClase.java
@@ -3,7 +3,11 @@ public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola, esto es un nuevo mensaje");
+		System.out.println("Hola, vamos a liarla parda");
+	}
+	public static void otroMetodo() {
+	System.out.println("otro mensaje");
+	}
 	}
 
 }
