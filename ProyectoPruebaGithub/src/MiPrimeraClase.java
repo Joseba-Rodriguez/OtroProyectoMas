fef40ec693ed09a3b7cd2cf5d22468efaa1b4db2
Joseba <joseba.rodriguez01@gmail.com>
2021-02-04 16:56:09 +0100

    Otro

--- ProyectoPruebaGithub/src/MiPrimeraClase.java
+++ ProyectoPruebaGithub/src/MiPrimeraClase.java
@@ -2,8 +2,9 @@
 public class MiPrimeraClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Esto es un mensaje");
 	}
-
+	private static void otroMetodo() {
+		System.out.println("Esto es otro metodo");
+	}
 }
